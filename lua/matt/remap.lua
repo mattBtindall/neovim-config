84643a95b11bcf0a7f8f5c12d6c57f7985d99405
mattbtindall <matthewbtindall@gmail.com>
2025-02-18 13:25:27 +0000

    add autosave and quit + save remap
    files now autosave when leaving the buffer and when losing focus of vim
    and when the user leaves with ':q'. Mapped ':q' to '<leader>q' and
    ':w' to '<leader>w'.

--- lua/matt/remap.lua
+++ lua/matt/remap.lua
@@ -73,5 +73,13 @@ vim.keymap.set("n", "<leader><leader>", function()
 end)
 
 -- my custom mappings
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "QuitPre" }, {
+    callback = function()
+        if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
+              vim.api.nvim_command('silent update')
+        end
+    end,
+})
 
-
+vim.keymap.set("n", "<leader>q", vim.cmd.q)
+vim.keymap.set("n", "<leader>w", vim.cmd.w)
