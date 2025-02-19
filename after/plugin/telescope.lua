b65ed6cfd4db7c034c4a89c5381ad3948f9f665f
mattbtindall <matthewbtindall@gmail.com>
2025-02-19 07:59:47 +0000

    add ability to find in all files by file type
    add a telescope live grep extension where flags can be passed
    https://github.com/nvim-telescope/telescope-live-grep-args.nvim
    to set file type. Change the search remaps to use this

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,23 @@
 local builtin = require('telescope.builtin')
+local telescope = require('telescope')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' }) -- find all files including node_modules
-vim.keymap.set('n', '<C-p>', builtin.git_files, {}) -- find only files that are tracked by git
-vim.keymap.set('n', '<leader>ps', function ()
-	builtin.grep_string({ string = vim.fn.input("Grep > ") });
-end)
+vim.keymap.set('n', '<C-p>', builtin.git_files, {}) -- find nly files that are tracked by git
+
+vim.keymap.set("v", "<leader>ps", function()
+  -- Get the selected text in visual mode
+  local function get_visual_selection()
+    vim.cmd('noau normal! "vy"') -- Yank selection into register v
+    return vim.fn.getreg('v') -- Get the yanked text
+  end
+
+  local selected_text = get_visual_selection()
+
+  -- Call live_grep_args with the selected text
+  require("telescope").extensions.live_grep_args.live_grep_args({ default_text = selected_text })
+end, { desc = "Live Grep Args (Find Selected Text)" })
+
+vim.keymap.set("n", "<leader>ps", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+
+telescope.setup({})
+telescope.load_extension("live_grep_args")
+
