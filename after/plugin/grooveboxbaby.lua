9502bf5a81cac867a0fdca2a202db8f93c3c431f
mattbtindall <matthewbtindall@gmail.com>
2025-02-19 07:56:35 +0000

    update theme to grooveboxbaby

---
+++ after/plugin/grooveboxbaby.lua
@@ -0,0 +1,7 @@
+-- Enable telescope theme
+-- vim.g.gruvbox_baby_telescope_theme builtin= 1
+-- Enable transparent mode
+vim.g.gruvbox_baby_transparent_mode = 1
+
+-- Load the colorscheme
+vim.cmd[[colorscheme gruvbox-baby]]
