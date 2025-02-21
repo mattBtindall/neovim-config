c5cf0c71a1f7edf4e5a0c168681ea7bc6a0ca589
mattbtindall <matthewbtindall@gmail.com>
2025-02-21 09:27:57 +0000

    Add telescope shortcute to open recent file

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,6 +3,20 @@ local telescope = require('telescope')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' }) -- find all files including node_modules
 vim.keymap.set('n', '<C-p>', builtin.git_files, {}) -- find nly files that are tracked by git
 
+-- opens recent files and goes to normal mode so can scroll straight away with jk keys
+vim.keymap.set('n', '<leader>pr', function()
+  require('telescope.builtin').oldfiles({
+    cwd_only = true,
+    attach_mappings = function(_, map)
+      -- Immediately switch to normal mode when the picker opens
+      vim.schedule(function()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), 'n', false)
+      end)
+      return true
+    end,
+  })
+end, { desc = "Recent files in project (normal mode)" })
+
 vim.keymap.set("v", "<leader>ps", function()
   -- Get the selected text in visual mode
   local function get_visual_selection()
@@ -18,6 +32,12 @@ end, { desc = "Live Grep Args (Find Selected Text)" })
 
 vim.keymap.set("n", "<leader>ps", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 
-telescope.setup({})
+telescope.setup({
+    defaults = {
+        path_display = {
+            "truncate"
+        }
+    }
+})
 telescope.load_extension("live_grep_args")
 
